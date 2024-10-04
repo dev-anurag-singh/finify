a5c293c2e8f902e9789bf07fc3f45904eace169c
Anurag <dev.anuragkr@gmail.com>
2024-10-04 20:54:35 +0530

    use create account hook

--- src/features/accounts/api/use-create-accounts.ts
+++ src/features/accounts/api/use-create-accounts.ts
@@ -1,12 +1,14 @@
 import { InferRequestType, InferResponseType } from 'hono';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { client } from '@/lib/hono';
+import { useToast } from '@/hooks/use-toast';
 
 type ResponseType = InferResponseType<typeof client.api.accounts.$post>;
 type RequestType = InferRequestType<typeof client.api.accounts.$post>['json'];
 
 export const useCreateAccount = () => {
   const queryClient = useQueryClient();
+  const { toast } = useToast();
 
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async json => {
@@ -14,8 +16,12 @@ export const useCreateAccount = () => {
       return await response.json();
     },
     onSuccess: () => {
+      toast({ title: 'Account Created' });
       queryClient.invalidateQueries({ queryKey: ['accounts'] });
     },
-    onError: () => {},
+    onError: () => {
+      toast({ title: 'Failed to create account', variant: 'destructive' });
+    },
   });
+  return mutation;
 };
