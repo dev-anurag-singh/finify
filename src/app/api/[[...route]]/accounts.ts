d56e244e914621871893354a8761a0970a4fe731
Anurag <dev.anuragkr@gmail.com>
2024-10-05 20:55:17 +0530

    updated accounts api route

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,8 +1,9 @@
 import { db } from '@/db/drizzle';
+import { z } from 'zod';
 import { accounts, insertAccountSchema } from '@/db/schema';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { createId } from '@paralleldrive/cuid2';
-import { eq } from 'drizzle-orm';
+import { and, eq, inArray } from 'drizzle-orm';
 import { Hono } from 'hono';
 import { zValidator } from '@hono/zod-validator';
 
@@ -24,6 +25,30 @@ const app = new Hono()
 
     return c.json({ data });
   })
+  .get(
+    '/:id',
+    clerkMiddleware(),
+    zValidator(
+      'param',
+      z.object({
+        id: z.string().min(4),
+      })
+    ),
+    async c => {
+      const auth = getAuth(c);
+      const { id } = c.req.valid('param');
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      if (!id) {
+        return c.json({ error: 'Id must be provided' }, 400);
+      }
+
+      return c.json({ data: {} });
+    }
+  )
   .post(
     '/',
     clerkMiddleware(),
@@ -49,6 +74,38 @@ const app = new Hono()
           ...values,
         })
         .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
+    '/delete-accounts',
+    clerkMiddleware(),
+    zValidator(
+      'json',
+      z.object({
+        ids: z.array(z.string()),
+      })
+    ),
+    async c => {
+      const auth = getAuth(c);
+      const values = c.req.valid('json');
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const data = await db
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.userId, auth.userId),
+            inArray(accounts.id, values.ids)
+          )
+        )
+        .returning({
+          id: accounts.id,
+        });
 
       return c.json({ data });
     }
