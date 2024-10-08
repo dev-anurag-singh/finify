c9c0b79f5470cc548436ea70bd3d79679b4e2d99
Anurag <dev.anuragkr@gmail.com>
2024-10-08 14:39:03 +0530

    added api to get account by id

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -25,30 +25,28 @@ const app = new Hono()
 
     return c.json({ data });
   })
-  .get(
-    '/:id',
-    clerkMiddleware(),
-    zValidator(
-      'param',
-      z.object({
-        id: z.string().min(4),
-      })
-    ),
-    async c => {
-      const auth = getAuth(c);
-      const { id } = c.req.valid('param');
+  .get('/:id', clerkMiddleware(), async c => {
+    const auth = getAuth(c);
+    const id = c.req.param('id');
 
-      if (!auth?.userId) {
-        return c.json({ error: 'Unauthorized' }, 401);
-      }
-
-      if (!id) {
-        return c.json({ error: 'Id must be provided' }, 400);
-      }
-
-      return c.json({ data: {} });
+    if (!auth?.userId) {
+      return c.json({ error: 'Unauthorized' }, 401);
     }
-  )
+
+    const [data] = await db
+      .select({
+        id: accounts.id,
+        name: accounts.name,
+      })
+      .from(accounts)
+      .where(and(eq(accounts.userId, auth.userId), eq(accounts.id, id)));
+
+    if (!data) {
+      return c.json({ error: 'Not Found' }, 404);
+    }
+
+    return c.json({ data });
+  })
   .post(
     '/',
     clerkMiddleware(),
