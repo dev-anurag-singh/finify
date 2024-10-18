15b2a13dfaf4de63d5cc7b8dc0a3aa1c333e8307
Anurag <dev.anuragkr@gmail.com>
2024-10-18 14:02:47 +0530

    edit account sheet

---
+++ src/features/accounts/components/edit-account-sheet.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+import { insertAccountSchema } from '@/db/schema';
+import AccountForm from './account-form';
+import { useCreateAccount } from '../api/use-create-accounts';
+import { z } from 'zod';
+import { useOpenAccount } from '@/features/accounts/hooks/use-open-account';
+import { useGetAccount } from '../api/use-get-account';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const formSchema = insertAccountSchema.pick({
+  name: true,
+});
+
+type FormValues = z.input<typeof formSchema>;
+
+function EditAccountSheet() {
+  const { isOpen, onClose, id } = useOpenAccount();
+  const { data, isLoading } = useGetAccount(id);
+  const { mutate, isPending } = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutate(values, {
+      onSuccess: () => onClose(),
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className='space-y-4'>
+        <SheetHeader>
+          <SheetTitle>Edit Account</SheetTitle>
+          <SheetDescription>Edit your account name</SheetDescription>
+        </SheetHeader>
+        {isLoading ? (
+          <Skeleton className='h-10 rounded-md mt-10' />
+        ) : (
+          <AccountForm
+            id={id}
+            onSubmit={onSubmit}
+            disabled={isPending}
+            defaultValues={data}
+          />
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default EditAccountSheet;
