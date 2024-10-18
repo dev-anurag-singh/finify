7536cef99d35370ba0559613e26159eef6c8aaa3
Anurag <dev.anuragkr@gmail.com>
2024-10-18 14:02:27 +0530

    updated get accounts hook

--- src/features/accounts/api/use-get-account.ts
+++ src/features/accounts/api/use-get-account.ts
@@ -2,13 +2,13 @@ import { useQuery } from '@tanstack/react-query';
 
 import { client } from '@/lib/hono';
 
-export const useGetAccount = (id?: string) => {
+export const useGetAccount = (id: string) => {
   const query = useQuery({
     enabled: !!id,
     queryKey: ['account', { id }],
     queryFn: async () => {
       const response = await client.api.accounts[':id'].$get({
-        param: { id: id! },
+        param: { id },
       });
 
       if (!response.ok) throw new Error('Failed to fetch account');
