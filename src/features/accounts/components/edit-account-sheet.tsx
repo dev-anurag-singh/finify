c782060624816291923c401a46c4ec38072edab2
Anurag <dev.anuragkr@gmail.com>
2024-10-23 01:50:36 +0530

    updated edit account to contain delete account modal

--- src/features/accounts/components/edit-account-sheet.tsx
+++ src/features/accounts/components/edit-account-sheet.tsx
@@ -12,40 +12,52 @@ import { useOpenAccount } from '@/features/accounts/hooks/use-open-account';
 import { useGetAccount } from '../api/use-get-account';
 import { Skeleton } from '@/components/ui/skeleton';
 import { useEditAccount } from '@/features/accounts/api/use-edit-account';
-import { useDeleteAccount } from '../api/use-delete-account';
+import { DeleteAccount } from './delete-account';
+import { useState } from 'react';
 
 function EditAccountSheet() {
   const { isOpen, onClose, id } = useOpenAccount();
   const { data, isLoading } = useGetAccount(id);
   const { mutate, isPending } = useEditAccount(id);
-  const { mutate: deleteAccount, isPending: isDeleting } = useDeleteAccount(id);
+  const [confirmOpen, setConfirmOpen] = useState(false);
 
   return (
-    <Sheet open={isOpen} onOpenChange={onClose}>
-      <SheetContent className='space-y-4'>
-        <SheetHeader>
-          <SheetTitle>Edit Account</SheetTitle>
-          <SheetDescription>Edit your account name</SheetDescription>
-        </SheetHeader>
-        {isLoading ? (
-          <Skeleton className='h-10 rounded-md mt-10' />
-        ) : (
-          <AccountForm
-            id={id}
-            onSubmit={values => {
-              mutate(values, {
-                onSuccess: onClose,
-              });
-            }}
-            onDelete={() => {
-              deleteAccount(undefined, { onSuccess: onClose });
-            }}
-            disabled={isPending || isDeleting}
-            defaultValues={data}
-          />
-        )}
-      </SheetContent>
-    </Sheet>
+    <>
+      <Sheet open={isOpen} onOpenChange={onClose}>
+        <SheetContent className='space-y-4'>
+          <SheetHeader>
+            <SheetTitle>Edit Account</SheetTitle>
+            <SheetDescription>Edit your account name</SheetDescription>
+          </SheetHeader>
+          {isLoading ? (
+            <Skeleton className='h-10 rounded-md mt-10' />
+          ) : (
+            <AccountForm
+              id={id}
+              onSubmit={values => {
+                mutate(values, {
+                  onSuccess: onClose,
+                });
+              }}
+              onDelete={() => {
+                // Open the confirmation dialog
+                setConfirmOpen(true);
+              }}
+              disabled={isPending}
+              defaultValues={data}
+            />
+          )}
+        </SheetContent>
+      </Sheet>
+      {/* Delete Account Modal */}
+      <DeleteAccount
+        open={confirmOpen}
+        onOpenChange={setConfirmOpen}
+        closeSheet={onClose}
+        id={data?.id!}
+        name={data?.name!}
+      />
+    </>
   );
 }
 
