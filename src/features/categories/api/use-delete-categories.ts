80d95ce91e63c1223317b415f35e4edb876c32d7
Anurag <dev.anuragkr@gmail.com>
2024-11-02 22:29:35 +0530

    added hook to delete multiple categories

---
+++ src/features/categories/api/use-delete-categories.ts
@@ -0,0 +1,36 @@
+import { InferRequestType, InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { client } from '@/lib/hono';
+import { useToast } from '@/hooks/use-toast';
+
+type ResponseType = InferResponseType<
+  (typeof client.api.categories)['delete-categories']['$post']
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.categories)['delete-categories']['$post']
+>['json'];
+
+export const useDeleteCategories = () => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async json => {
+      const response = await client.api.categories['delete-categories'][
+        '$post'
+      ]({
+        json,
+      });
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast({ title: 'Categories deleted' });
+      queryClient.invalidateQueries({ queryKey: ['categories'] });
+      //   TODO: Also invalidate summary
+    },
+    onError: () => {
+      toast({ title: 'Failed to delete categories', variant: 'destructive' });
+    },
+  });
+  return mutation;
+};
