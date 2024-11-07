7d0dae1f1441213ca65dd02098bc2cfb2b12f6f0
Anurag <dev.anuragkr@gmail.com>
2024-11-07 20:46:12 +0530

    added endpoint to create multiple transactions at once

--- src/app/api/[[...route]]/transactions.ts
+++ src/app/api/[[...route]]/transactions.ts
@@ -127,6 +127,31 @@ const app = new Hono()
     }
   )
   .post(
+    '/create-transactions',
+    clerkMiddleware(),
+    zValidator('json', z.array(insertTransactionSchema.omit({ id: true }))),
+    async c => {
+      const auth = getAuth(c);
+      const values = c.req.valid('json');
+
+      if (!auth?.userId) {
+        return c.json({ error: 'Unauthorized' }, 401);
+      }
+
+      const data = await db
+        .insert(transactions)
+        .values(
+          values.map(value => ({
+            id: createId(),
+            ...value,
+          }))
+        )
+        .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
     '/delete-transactions',
     clerkMiddleware(),
     zValidator(
