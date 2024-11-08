bf6794d6ad239109a44afdbf196fc766f2185327
Anurag <dev.anuragkr@gmail.com>
2024-11-08 20:57:38 +0530

    hook to update a transaction

---
+++ src/features/transactions/api/use-edit-transaction.ts
@@ -0,0 +1,36 @@
+import { InferRequestType, InferResponseType } from 'hono';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { client } from '@/lib/hono';
+import { useToast } from '@/hooks/use-toast';
+
+type ResponseType = InferResponseType<
+  (typeof client.api.transactions)[':id']['$patch']
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.transactions)[':id']['$patch']
+>['json'];
+
+export const useEditTransaction = (id: string) => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async json => {
+      const response = await client.api.transactions[':id']['$patch']({
+        param: { id },
+        json,
+      });
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast({ title: 'Transaction Updated' });
+      queryClient.invalidateQueries({ queryKey: ['transactions'] });
+      queryClient.invalidateQueries({ queryKey: ['transaction', { id }] });
+    },
+    onError: () => {
+      toast({ title: 'Failed to update transaction', variant: 'destructive' });
+    },
+  });
+  return mutation;
+};
