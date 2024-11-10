38ae426a7c806895bf05f5e96b14025ffac64c45
Anurag <dev.anuragkr@gmail.com>
2024-11-10 23:38:03 +0530

    added new transaction sheet to sheet provider

--- src/providers/sheet-provider.tsx
+++ src/providers/sheet-provider.tsx
@@ -2,6 +2,7 @@ import NewAccountSheet from '@/features/accounts/components/new-account-sheet';
 import EditAccountSheet from '@/features/accounts/components/edit-account-sheet';
 import NewCategorySheet from '@/features/categories/components/new-category-sheet';
 import EditCategorySheet from '@/features/categories/components/edit-category-sheet';
+import NewTransactionSheet from '@/features/transactions/components/new-transaction-sheet';
 
 export function SheetProvider() {
   return (
@@ -10,6 +11,7 @@ export function SheetProvider() {
       <EditAccountSheet />
       <NewCategorySheet />
       <EditCategorySheet />
+      <NewTransactionSheet />
     </>
   );
 }
