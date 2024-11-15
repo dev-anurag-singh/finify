e8d1177ed6de66949e65f18f06b7f4a346dd0f00
Anurag <dev.anuragkr@gmail.com>
2024-11-16 02:24:21 +0530

    sheet to add new transation

---
+++ src/features/transactions/components/new-transaction-sheet.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from '@/components/ui/sheet';
+import { useNewTransaction } from '../hooks/use-new-transaction';
+import { useCreateTransaction } from '../api/use-create-transaction';
+import TransactionForm from './transaction-form';
+
+function NewTransactionSheet() {
+  const { isOpen, onClose } = useNewTransaction();
+  const { mutate, isPending } = useCreateTransaction();
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className='space-y-4'>
+        <SheetHeader>
+          <SheetTitle>New Transaction</SheetTitle>
+          <SheetDescription>Add a new transaction.</SheetDescription>
+        </SheetHeader>
+        <TransactionForm onSubmit={values => mutate(values)} disabled={isPending} />
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+export default NewTransactionSheet;
+
+// 6:34:46
