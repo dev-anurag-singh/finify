1e6411c8e077d9bb097794201a8d67c0bd769147
Anurag <dev.anuragkr@gmail.com>
2024-11-20 02:15:15 +0530

    included currency conversion

--- src/features/transactions/api/use-get-transaction.ts
+++ src/features/transactions/api/use-get-transaction.ts
@@ -1,8 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { client } from '@/lib/hono';
+import { convertAmountFromMiliunits } from '@/lib/utils';
 
-export const useGetTransation = (id: string) => {
+export const useGetTransaction = (id: string) => {
   const query = useQuery({
     enabled: !!id,
     queryKey: ['transaction', { id }],
@@ -15,7 +16,10 @@ export const useGetTransation = (id: string) => {
 
       const { data } = await response.json();
 
-      return data;
+      return {
+        ...data,
+        amount: convertAmountFromMiliunits(data.amount),
+      };
     },
   });
 
