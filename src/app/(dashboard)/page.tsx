8b901c122472daaa75623d2ade751e02527440bd
Anurag <dev.anuragkr@gmail.com>
2024-12-17 23:38:03 +0530

    modified dashboard page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,9 @@
+import { DataGrid } from '@/components/data-grid';
+
 function DashboardPage() {
   return (
     <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
-      {/* <DataGrid /> */}
+      <DataGrid />
     </div>
   );
 }
