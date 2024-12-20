b21d7e9a0cad29cde8cb366079c7062365c98ed8
Anurag <dev.anuragkr@gmail.com>
2024-12-21 01:40:34 +0530

    added formatPercentage util fn

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,6 +22,22 @@ export function formatCurrency(value: number) {
   }).format(value);
 }
 
+export function formatPercentage(
+  value: number,
+  options: {
+    addPrefix?: boolean;
+  } = { addPrefix: false }
+) {
+  const result = new Intl.NumberFormat('en-US', {
+    style: 'percent',
+  }).format(value / 100);
+
+  if (options.addPrefix && value > 0) {
+    return `+${result}`;
+  }
+  return result;
+}
+
 export function calculatePercentageChange(current: number, previous: number) {
   if (previous === 0) {
     return previous === current ? 0 : 100;
