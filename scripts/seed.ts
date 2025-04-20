dc3dba2dc4d68f7ff1bd6e244d3f1f58df2732ed
Anurag <dev.anuragkr@gmail.com>
2025-04-20 15:13:25 +0530

    updated seed script

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -1,68 +1,69 @@
-import { config } from 'dotenv';
-import { subDays } from 'date-fns';
-import { neon } from '@neondatabase/serverless';
-import { categories, transactions, accounts } from '@/db/schema';
-import { drizzle } from 'drizzle-orm/neon-http';
-import { eachDayOfInterval, format } from 'date-fns';
-import { convertAmountToMiliunits } from '@/lib/utils';
+import { config } from "dotenv";
+import { addDays, subDays } from "date-fns";
+import { neon } from "@neondatabase/serverless";
+import { categories, transactions, accounts } from "@/db/schema";
+import { drizzle } from "drizzle-orm/neon-http";
+import { eachDayOfInterval, format } from "date-fns";
+import { convertAmountToMiliunits } from "@/lib/utils";
 
-config({ path: '.env.local' });
+config({ path: ".env.local" });
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle(sql);
 
-const SEED_USER_ID = 'user_2u7Wu8GPdjNdRo08n78BK1jcJLU';
+const SEED_USER_ID = "user_2u7Wu8GPdjNdRo08n78BK1jcJLU";
 
 const SEED_CATEGORIES = [
-  { id: 'category_1', name: 'Food', userId: SEED_USER_ID, plaidID: null },
-  { id: 'category_2', name: 'Rent', userId: SEED_USER_ID, plaidID: null },
-  { id: 'category_3', name: 'Utilities', userId: SEED_USER_ID, plaidID: null },
-  { id: 'category_4', name: 'Clothing', userId: SEED_USER_ID, plaidID: null },
-  { id: 'category_5', name: 'Health', userId: SEED_USER_ID, plaidID: null },
+  { id: "category_1", name: "Food", userId: SEED_USER_ID, plaidID: null },
+  { id: "category_2", name: "Rent", userId: SEED_USER_ID, plaidID: null },
+  { id: "category_3", name: "Utilities", userId: SEED_USER_ID, plaidID: null },
+  { id: "category_4", name: "Clothing", userId: SEED_USER_ID, plaidID: null },
+  { id: "category_5", name: "Health", userId: SEED_USER_ID, plaidID: null },
   {
-    id: 'category_6',
-    name: 'Transportation',
+    id: "category_6",
+    name: "Transportation",
     userId: SEED_USER_ID,
     plaidID: null,
   },
   {
-    id: 'category_7',
-    name: 'Entertainment',
+    id: "category_7",
+    name: "Entertainment",
     userId: SEED_USER_ID,
     plaidID: null,
   },
   {
-    id: 'category_8',
-    name: 'Miscellaneous',
+    id: "category_8",
+    name: "Miscellaneous",
     userId: SEED_USER_ID,
     plaidID: null,
   },
 ];
 
 const SEED_ACCOUNTS = [
-  { id: 'account_1', name: 'Checking', userId: SEED_USER_ID, plaidID: null },
-  { id: 'account_2', name: 'Savings', userId: SEED_USER_ID, plaidID: null },
+  { id: "account_1", name: "Checking", userId: SEED_USER_ID, plaidID: null },
+  { id: "account_2", name: "Savings", userId: SEED_USER_ID, plaidID: null },
 ];
 
-const defaultTo = new Date();
-const defaultFrom = subDays(defaultTo, 90);
+const defaultDate = new Date();
+const defaultTo = addDays(defaultDate, 90);
+const defaultFrom = subDays(defaultDate, 90);
 
 const SEED_TRANSACTIONS: (typeof transactions.$inferInsert)[] = [];
 
 const generateRandomAmount = (category: typeof categories.$inferInsert) => {
   switch (category.name) {
-    case 'Food':
+    case "Food":
       return Math.random() * 30 + 10;
-    case 'Rent':
+    case "Rent":
       return Math.random() * 400 + 90;
-    case 'Utilities':
+    case "Utilities":
       return Math.random() * 200 + 50;
-    case 'Clothing':
-    case 'Entertainment':
-    case 'Miscellaneous':
+    case "Clothing":
+    case "Entertainment":
+    case "Miscellaneous":
       return Math.random() * 100 + 20;
-    case 'Health':
-    case 'Transportation':
+    case "Health":
+    case "Transportation":
       return Math.random() * 50 + 15;
     default:
       return Math.random() * 50 + 10;
@@ -81,12 +82,12 @@ const generateTransactionsForDay = (day: Date) => {
     );
 
     const transaction: typeof transactions.$inferInsert = {
-      id: `transaction_${format(day, 'yyyy-MM-dd')}_${i}`,
+      id: `transaction_${format(day, "yyyy-MM-dd")}_${i}`,
       categoryId: category.id,
       accountId: !isExpense ? SEED_ACCOUNTS[1].id : SEED_ACCOUNTS[0].id,
       amount: formattedAmount,
-      payee: 'Merchant',
-      notes: 'Random transaction',
+      payee: "Merchant",
+      notes: "Random transaction",
       date: day,
     };
     SEED_TRANSACTIONS.push(transaction);
@@ -112,7 +113,7 @@ const main = async () => {
     await db.insert(accounts).values(SEED_ACCOUNTS).execute();
     await db.insert(transactions).values(SEED_TRANSACTIONS).execute();
   } catch (error) {
-    console.log('error during seed', error);
+    console.log("error during seed", error);
     process.exit(1);
   }
 };
